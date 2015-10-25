f16faac8fed857b989ba2eb96b678bab1dba1565
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-10-25 16:34:56 -0200

    Initial commit

---
+++ SoT/SoT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SoT
+//
+//  Created by Jhonathan Wyterlin on 25/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
