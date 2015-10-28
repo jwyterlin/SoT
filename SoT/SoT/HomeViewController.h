bb24ace5aea6692a979ba1e2fb84c13d22654837
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-10-28 13:21:20 -0200

    Renaming ViewController to HomeViewController

--- SoT/SoT/HomeViewController.h
+++ SoT/SoT/HomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeViewController.h
 //  SoT
 //
 //  Created by Jhonathan Wyterlin on 25/10/15.
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface HomeViewController : UIViewController
 
 
 @end
---
+++ SoT/SoT/HomeViewController.m
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.m
+//  SoT
+//
+//  Created by Jhonathan Wyterlin on 25/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController()
+
+@end
+
+@implementation HomeViewController
+
+#pragma mark - View Lifecycle
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
