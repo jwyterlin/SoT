c92b728158d37b9864e079a6458786169a322c2e
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-10-29 12:10:12 -0200

    Created GenericTableViewCell

---
+++ SoT/SoT/Custom UI Components/UITableViewCell/GenericTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GenericTableViewCell.h
+//  SoT
+//
+//  Created by Jhonathan Wyterlin on 29/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GenericTableViewCell : UITableViewCell
+
+@end
---
+++ SoT/SoT/Custom UI Components/UITableViewCell/GenericTableViewCell.m
@@ -0,0 +1,20 @@
+//
+//  GenericTableViewCell.m
+//  SoT
+//
+//  Created by Jhonathan Wyterlin on 29/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericTableViewCell.h"
+
+@implementation GenericTableViewCell
+
+-(void)awakeFromNib {
+}
+
+-(void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
