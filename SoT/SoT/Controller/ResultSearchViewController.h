7258bab6888dc0d6cd1a0e5540510f42fe364d73
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-10-29 11:53:13 -0200

    Created ResultSearchViewController

---
+++ SoT/SoT/Controller/ResultSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultSearchViewController.h
+//  SoT
+//
+//  Created by Jhonathan Wyterlin on 29/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericViewController.h"
+
+@interface ResultSearchViewController : GenericViewController
+
+@end
---
+++ SoT/SoT/Controller/ResultSearchViewController.m
@@ -0,0 +1,27 @@
+//
+//  ResultSearchViewController.m
+//  SoT
+//
+//  Created by Jhonathan Wyterlin on 29/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ResultSearchViewController.h"
+
+@interface ResultSearchViewController()
+
+@end
+
+@implementation ResultSearchViewController
+
+#pragma mark - View Lifecycle
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
